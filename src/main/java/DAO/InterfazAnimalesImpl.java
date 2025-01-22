5629b22b2537e28e5b24f65d4fe19250906abc3a
Andrea Catalán Menacho <catalanmenachoandrea@gmail.com>
2025-01-22 20:07:44 +0100

    Implementaciones Interfaces

---
+++ src/main/java/DAO/InterfazAnimalesImpl.java
@@ -0,0 +1,37 @@
+package DAO;
+
+import entities.Animales;
+
+import java.util.List;
+
+public class InterfazAnimalesImpl implements InterfazAnimales {
+    @Override
+    public List<Animales> findAll() {
+        return null;
+    }
+
+    @Override
+    public Animales findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Animales> findByName(String name) {
+        return null;
+    }
+
+    @Override
+    public Animales create(Animales animales) {
+        return null;
+    }
+
+    @Override
+    public Animales update(Animales animales) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(int id) {
+        return false;
+    }
+}
---
+++ src/main/java/DAO/InterfazUsuariosImpl.java
@@ -0,0 +1,37 @@
+package DAO;
+
+import entities.Usuarios;
+
+import java.util.List;
+
+public class InterfazUsuariosImpl implements InterfazUsuarios {
+    @Override
+    public List<Usuarios> findAll() {
+        return null;
+    }
+
+    @Override
+    public Usuarios findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Usuarios> findByName(String name) {
+        return null;
+    }
+
+    @Override
+    public Usuarios create(Usuarios persona) {
+        return null;
+    }
+
+    @Override
+    public Usuarios update(Usuarios persona) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(int id) {
+        return false;
+    }
+}
